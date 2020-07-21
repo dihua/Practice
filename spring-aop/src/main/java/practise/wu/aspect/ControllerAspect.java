39f90b3274ca7cc0acafae97af562b33c49338b4
dihua.wu <wdhua_j@163.com>
2020-07-21 14:24:54 +0800

    add comments

--- spring-aop/src/main/java/practise/wu/aspect/ControllerAspect.java
+++ spring-aop/src/main/java/practise/wu/aspect/ControllerAspect.java
@@ -21,6 +21,13 @@ public class ControllerAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ControllerAspect.class);
 
+    /**
+     * 可以统一定义切点
+     * 第1个 * 表示要拦截的目标方法返回值可以是任意的【也可以明确指明返回类型】
+     * 第2个 * 表示controller包下的任意类
+     * 第3个 * 表示类中的任意方法
+     * 最后 .. 表示方法参数任意，个数任意，类型任意
+     */
     @Pointcut("execution(* practise.wu.controller.*.*(..))")
     private void controllerPointCut () {
 
